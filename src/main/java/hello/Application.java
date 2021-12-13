a446680c5098e11d518fe85bdb5cc6d78e80a040
Lukasz Druminski <druminski@gmail.com>
2021-12-13 11:04:20 +0000

    init

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,114 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  static class Self {
+    public String href;
+  }
+
+  static class Links {
+    public Self self;
+  }
+
+  static class PlayerState {
+    public Integer x;
+    public Integer y;
+    public String direction;
+    public Boolean wasHit;
+    public Integer score;
+  }
+
+  static class Arena {
+    public List<Integer> dims;
+    public Map<String, PlayerState> state;
+  }
+
+  static class ArenaUpdate {
+    public Links _links;
+    public Arena arena;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    binder.initDirectFieldAccess();
+  }
+
+  @GetMapping("/")
+  public String index() {
+    return "Let the battle begin!";
+  }
+
+  @PostMapping("/**")
+  public String index(@RequestBody ArenaUpdate arenaUpdate) {
+    System.out.println(arenaUpdate);
+
+    // find mine position
+    PlayerState myState = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
+    
+    // is someone on target
+    if (myState.direction == "N") {
+        for (Map.Entry<String, PlayerState> entry : arenaUpdate.arena.state.entrySet()) {
+            if (entry.getValue().x == myState.x && myState.y - entry.getValue().y <= 3 && myState.y - entry.getValue().y >= 1) {
+                return "T";
+            }
+        }
+        if (myState.y > arenaUpdate.arena.dims.get(0) + 2) {
+            return "F";
+        }
+    }
+    else if (myState.direction == "S") {
+        for (Map.Entry<String, PlayerState> entry : arenaUpdate.arena.state.entrySet()) {
+            if (entry.getValue().x == myState.x && entry.getValue().y - myState.y <= 3 && entry.getValue().y - myState.y >= 1) {
+                return "T";
+            }
+        }
+        if (myState.x < arenaUpdate.arena.dims.get(1) - 2) {
+            return "F";
+        }
+    }
+    else if (myState.direction == "E") {
+        for (Map.Entry<String, PlayerState> entry : arenaUpdate.arena.state.entrySet()) {
+            if (entry.getValue().y == myState.y && entry.getValue().x - myState.x <= 3 && entry.getValue().x - myState.x >= 1) {
+                return "T";
+            }
+        }
+        if (myState.x < (arenaUpdate.arena.dims.get(0) - 2)) {
+            return "F";
+        }
+    }
+    else if (myState.direction == "W") {
+        for (Map.Entry<String, PlayerState> entry : arenaUpdate.arena.state.entrySet()) {
+            if (entry.getValue().y == myState.y && myState.x - entry.getValue().x <= 3 && myState.x - entry.getValue().x >= 1) {
+                return "T";
+            }
+        }
+        if (myState.x > arenaUpdate.arena.dims.get(0) + 2) {
+            return "F";
+        }
+    }
+    
+    return "R";
+
+    // if (myState.x < arenaUpdate.arena.dims.get(0))
+    // String[] commands = new String[]{"F", "R", "L"};
+    // int i = new Random().nextInt(3);
+    // return commands[i];
+  }
+
+}
+
